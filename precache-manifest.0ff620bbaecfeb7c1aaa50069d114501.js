ca04efefb7bdf77c3391fc69e4c8ede6285a4aa0
Rahul <rahulkr1413@gmail.com>
2020-05-27 11:03:28 +0530

    Updates

---
+++ precache-manifest.0ff620bbaecfeb7c1aaa50069d114501.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd85396aad0f0543d4ad866c7857180",
+    "url": "/roboWorld/index.html"
+  },
+  {
+    "revision": "9e6f6b0050aad7ae355d",
+    "url": "/roboWorld/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cb0c4ae61fc437f9a1c6",
+    "url": "/roboWorld/static/css/main.db2e253a.chunk.css"
+  },
+  {
+    "revision": "9e6f6b0050aad7ae355d",
+    "url": "/roboWorld/static/js/2.2082894f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboWorld/static/js/2.2082894f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0c4ae61fc437f9a1c6",
+    "url": "/roboWorld/static/js/main.6b0b560c.chunk.js"
+  },
+  {
+    "revision": "c7bed04e1d62c84e0224",
+    "url": "/roboWorld/static/js/runtime-main.fb277cc5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboWorld/static/media/SEGA.12a3db8b.woff"
+  }
+]);
